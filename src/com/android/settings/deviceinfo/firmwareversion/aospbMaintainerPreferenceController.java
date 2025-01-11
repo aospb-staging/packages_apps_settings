2815fc95c4e509207ac1295a310cef7089378a8e
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-01-11 13:34:34 +0000

    Settings: Pull maintainer summary from ro.aospb.maintainer prop
    Change-Id: Iaac1a5b63e7ba4b300ee69b8dc0b3419537fbaeb

--- src/com/android/settings/deviceinfo/firmwareversion/aospbMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/aospbMaintainerPreferenceController.java
@@ -17,7 +17,7 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
-import android.content.res.Resources;
+import android.os.SystemProperties;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -28,6 +28,7 @@ import com.android.settings.core.BasePreferenceController;
 public class aospbMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "aospbMaintainerPreferenceController";
+    private static final String MAINTAINER_STRING = "ro.aospb.maintainer";
 
     public aospbMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -38,7 +39,8 @@ public class aospbMaintainerPreferenceController extends BasePreferenceControlle
     }
 
     public CharSequence getSummary() {
-        String maintainer = mContext.getResources().getString(R.string.aospb_maintainer);
+        String maintainer = SystemProperties.get(MAINTAINER_STRING,
+                this.mContext.getString(R.string.device_info_default));
         return maintainer;
     }
 }
